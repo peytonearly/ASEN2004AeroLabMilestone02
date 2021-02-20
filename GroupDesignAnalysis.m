51bbef4b36d480168fc28f0e820e8edf367bd9f6
Peyton Early <64031475+peytonearly@users.noreply.github.com>
2021-02-20 14:36:52 -0700

    Created frame for group design analysis

---
+++ GroupDesignAnalysis.m
@@ -0,0 +1,133 @@
+%% ----------------------- Team Glider Design -------------------------- %%
+%% Cleaning
+clear;
+clc;
+close all;
+%% Variables
+% Provided flat plate CL and CD values with respect to AOA
+plateCl = [-.75 -.76 -.77 -.78 -0.8 -0.78 -.76 -.74 -.7 -.6 -.5 -.38 -.3 -.2 -.1 0 .1 .2 .3 .4 .49 .6 .7 .75 .76 .77 .76 .75 .75 .74 .74 .73 .72 .71 .71 .7];
+plateCd = [.21 .2 .19 .18 .17 .15 .13 .11 .09 .07 .05 .04 .02 .01 .005 0  .005 .01 .015 .02 .05 .065 0.09 .11 .13 .15 .17 .19 .199 .21 .22 .23 .25 .27 .285 .29];
+plateAOA = [-15:1:20];
+ReCr = 140000;
+
+% Air properties assuming altiitude of 1500 m
+mu      = 1.74 * 10^-5;                                                    % Air viscosity [kg/m*s]
+rhoAlt  = 1.0581;                                                          % Air density [kg/m^3]
+ReL     = 1000000;                                                         % Local Reynold's value (can't remember where this number is from - maybe milestone 1
+rhoFoam = 0.295 / 1000;                                                    % Density of Dollar Tree foam [kg/m^3]
+g       = 9.81;                                                            % Acceleration of gravity [m/s^2]
+
+% Design properties
+h        = 7;                                                              % Initial launch height[m]
+b        = nan;                                                            % Wing span [m]
+AR       = nan;                                                            % Aspect ratio
+Swet     = nan;                                                            % Wetted area [m^2]
+Sref     = nan;                                                            % Wing planform area [m^2]
+Srest    = nan;                                                            % Surface area of fuselage and tail [m^2]
+Sh       = nan;                                                            % Planform area of horizontal tail [m^2]
+c        = nan;                                                            % Chord of wing [m]
+cBar     = Sref / b;                                                       % Mean aerodynamic chord [m]
+cr       = nan;                                                            % Root chord [m]
+ct       = nan;                                                            % Tip chord [m]
+lambda   = ct / cr;                                                        % Taper ratio
+mass     = nan;                                                            % Weight [kg]
+e        = 0.9;                                                            % Estimated span efficiency factor
+% e0       = (1.78*(1-(0.045*(AR^0.68)))) - 0.64;                            % Oswald's efficiency factor
+e0       = 1;                                                              % Oswald's efficiency factor
+V        = 7;                                                              % Velocity [m/s]
+Wfuse    = nan;                                                            % Fuselage weight [N]
+WHTail   = nan;                                                            % Horizontal tail (?) weight [N]
+WVTail   = nan;                                                            % Vertical tail (?) weight [N]
+WBallast = nan;                                                            % Ballast weight [N]
+WPay     = nan;                                                            % Payload weight [N]
+Wwing    = Sref*rhoFoam*g;                                                 % Wing weight [N]
+Wrest    = Wfuse + WHTail + WVTail + WBallast + WPay;                      % Weight of glider without wings [N]
+% WTO      = Wfuse + Wwing + WHTail + WVTail + WBallast + WPay;              % Takeoff weight [N]
+WTO      = Wrest + Wwing;                                                  % Takeoff weight [N]
+WingLoad = WTO / Sref;                                                     % Wing loading [N]
+EPitCon  = nan;                                                            % Elevator pitch control [deg] (+/- 10 deg)
+xCg      = nan;                                                            % Longitudinal stability center of gravity location [m] (0c < xCg < 1.0c)
+xACv     = nan;                                                            % Vertical tail aerodynamic center [m]
+Vh       = nan;                                                            % Horizontal tail volume [m^3] (0.3 < Vh < 0.6)
+Vv       = nan;                                                            % Vertial tail volume [m^3] (0.02 < Vv < 0.05)
+B        = nan;                                                            % Spiral parameter (B > 5)
+dAngle   = nan;                                                            % Dihedral angle [deg]
+ep0      = 0;                                                              % Downwash on tail at L = 0
+iT       = nan;                                                            % Horizontal tail incident angle [deg]
+angTail  = nan;                                                            % Tail angle of attack [deg]
+Cmcg     = nan;                                                            % Coefficient of moment about center of gravity (=0 for trimmed flight)
+Cmacw    = nan;                                                            % Coefficient of moment about wing aerodynamic center (approximate with 2D airfoil value)
+CLw      = nan;                                                            % Coefficient of lift for wing at max range or max endurance (= to whole aircraft CL)
+CLht     = nan;                                                            % Coefficient of lift for horizontal tail (flat plate approx)
+
+%% ----------------------------- Analysis ------------------------------ %%
+%% Estimated Lift Curve
+fit2D = polyfit(plateAOA, plateCl, 1);                                     % Best fit line for Cl vs AOA
+a0 = fit2D(1);                                                             % Save value of slope
+fit2D = polyfit(plateCl, plateAOA, 1);                                     % Best fit line for AOA vs Cl (for finding AOA @ L=0)
+angle0lift = polyval(fit2D, 0);                                            % Angle of attack at 0 lift
+a = a0 / (1 + (57.3 * a0)/(pi * e * AR));                                  % 3D lift slope
+calcCL = a .* (plateAOA - angle0lift);                                     % 3D coefficient of lift
+
+%% Whole Aircraft Drag Polar
+xCr = (ReCr * mu) / (rhoAlt * V);
+CfTurbTotal = 0.074/(ReL^0.2);                                             % Total Wing Turbulent Cf
+CfTurbLeft = 0.0592/(ReCr^0.2);                                            % Turbulent Cf to be subtracted from total
+CfLamLeft = 0.664/sqrt(ReCr);                                              % Laminar Cf
+CfTotal = 2*(CfTurbTotal - CfTurbLeft + CfLamLeft);                        % Total Cf for both wings
+
+CDMin = CfTotal * (Swet / Sref);
+CDwing = plateCd + (calcCL.^2 / (pi * e * AR));
+fitDrag = polyfit(CDwing, calcCL,2 );                                      % Best fit line for finding CLminD
+CLminD = polyval(fitDrag, min(CDwing));                                    % CL at min drag
+CD0 = CDMin + (CLminD^2 / (pi*e*AR));                                      % Parasite drag
+CD = CD0 + (calcCL.^2 / (pi*e*AR));                                        % Calculate whole aircraft coefficient of drag
+
+%% Weight/Mass Estimation and Sizing
+
+
+%% Longitudinal Static Stability and Trim
+
+
+%% Lateral Directional Stability
+
+
+%% Estimated Performance
+LoD = calcCL ./ CD;                                                        % Find L/D values
+LoDMax = max(LoD);                                                         % Find maximum L/D value
+maxCL = calcCL(LoD == LoDMax);                                             % CL at L/D max
+maxCD = CD(LoD == LoDMax);                                                 % CD at L/D max
+theta = maxCD / maxCL;                                                     % Glide angle [deg]
+sinkRate = V * sin(theta);                                                 % Sink rate of glider [m/s]
+
+maxRange = h * LoDMax;                                                     % Max glide range [m]
+maxRVel = sqrt(2*WingLoad ./ (maxCL * rhoAlt * Sref));                     % Max range velocity [m/s]
+maxREndur = h / sinkRate;                                                  % Glide endurance [s]
+
+%% Graphs
+% Lift Curve Plots
+figure();
+plot(plateAOA, plateCl)
+hold on;
+plot(plateAOA, calcCL)
+yline(0);
+xline(0);
+hold off;
+grid on;
+title('Estimated Lift Curves')
+xlabel('Angle of Attack [deg]')
+ylabel('Coefficient of Lift')
+legend('2D Approx.', '3D Approx.', 'Location', 'Northwest')
+
+% Whole Aircraft Drag Polar
+figure();
+plot(plateAOA, CD)
+grid on;
+title('Whole Aircraft Drag Polar')
+xlabel('Angle of Attack [deg]')
+ylabel('Coefficient of Drag')
+
+%% Outputs
+cost = mass * 1000;
+summary = table(maxRange, maxRVel, maxREndur, EPitCon, xCg, Vh, Vv, B, b, cost);
+summary.Properties.VariableNames = {'MaxGlideRange', 'MaxGlideRangeVelocity', 'MaxGlideEndurance', 'ElevatorPitchControl', 'XcgLocation', 'HorzTailVolume', 'VertTailVolume', 'Spiral', 'Wingspan', 'Cost'};
---
+++ TylerCode.m
@@ -0,0 +1,176 @@
+clear
+clc
+
+% %% Equations
+% rho = 1.0581; % density at alt 1.5 km (kg/m^3)
+% AR = nan;
+% rhoM = 0.295; % g/m^3
+% g = 9.81;
+% e = 0.9;
+% e0 = (1.78*(1-(0.045*(AR^0.68)))) - 0.64;
+% h = 7;
+% 
+% % Weights
+% syms Sref
+% Wfuse = nan;
+% WHTail = nan;
+% WVTail = nan;
+% WBallast = nan;
+% WPay = nan;
+% Wwing = Sref*rhoM*g;
+% % 2
+% Wrest = Wfuse + WHTail + WVTail + WBallast + WPay;
+% WTO = Wfuse + Wwing + WHTail + WVTail + WBallast + WPay;
+% % WTO = Wrest + Sref*rhoM*g
+% % WingLoad = WTO/Sref = Wrest/Sref+rhoM*g
+% 
+% % Lift
+% alpha = -10:20;
+% a0 = nan;
+% alpha0 = 0;
+% a = a0/(1+a0/(pi*AR*e));
+% CL = a*(alpha-alpha0);
+% 
+% % Drag
+% Swet = nan;
+% Cfe = 0.0030;
+% CDmin = Cfe*(Swet/Sref);
+% % 1
+% CD0 = CDmin;
+% CD = CD0 + CL.^2/(pi*e0*AR);
+% % CD = Cfe*(Swet/Sref) + CL(alpha)^2/(pi*e0*AR)
+% 
+% 
+% 
+% %% Performance Graphs
+% syms WingLoad
+% % Range
+% % Rmax = h*LDmax
+% % Rmax = h*CL(AOALDmaxIndex)/(Cfe*(Swet/Sref) + CL(AOALDmaxIndex)^2/(pi*e0*AR))
+% Rmax = h*CL(AOALDmaxIndex)/(WingLoad*Cfe*Swet/WTO + CL(AOALDmaxIndex)^2/(pi*e0*AR));
+% % vRmax = sqrt(2*WTO/(CL(AOALDmaxIndex)*Sref*rho))
+% % vRmax = sqrt((WTO/Sref)*2/(CL(AOALDmaxIndex)*rho))
+% vRmax = sqrt(WingLoad*2/(CL(AOALDmaxIndex)*rho));
+% % Endurance
+% % vEmax = sqrt(2*WTO/(CL(AOAPRminIndex)*Sref*rho))
+% % vEmax = sqrt(WTO/Sref*2/(CL(AOAPRminIndex)*rho))
+% vEmax = sqrt(WingLoad*2/(CL(AOAPRminIndex)*rho));
+% % DEmax = CDPRmin*1/2*vEmax^2*rho*Sref
+% % DEmax = (Cfe*(Swet/Sref) + CL(AOAPRminIndex)^2/(pi*e0*AR))*1/2*(WTO/Sref*2/(CL(AOAPRminIndex)*rho))*rho*Sref
+% % DEmax = (Cfe*(Swet/Sref) + CL(AOAPRminIndex)^2/(pi*e0*AR))*(WTO/(CL(AOAPRminIndex)))
+% DEmax = (WingLoad*Cfe*Swet/CL(AOAPRminIndex) + WTO*CL(AOAPRminIndex))/(pi*e0*AR);
+% % sink = vEmax*DEmax/WTO
+% % sink = sqrt(WingLoad*2/(CL(AOALDmaxIndex)*rho))*((WingLoad*Cfe*Swet/CL(AOAPRminIndex) + WTO*CL(AOAPRminIndex))/(pi*e0*AR))/WTO
+% sink = sqrt(WingLoad*2/(CL(AOALDmaxIndex)*rho))*((1/Sref*Cfe*Swet/CL(AOAPRminIndex) + CL(AOAPRminIndex))/(pi*e0*AR));
+% Emax = h/sink;
+
+% Initializations
+h = 7;
+% b = 1; % (max) wingspan
+% AR = b^2/Sref; % contsant!
+AR = 10;
+e0 = 1;
+Cfe = 0.0030;
+k = 1/(pi*AR*e0);
+rhoM = 0.295; % density of foam (N/m^2)???
+rho = 1.0581; % density at alt 1.5 km (kg/m^3)
+Wrest = 0.5; % weight of glider minus wings (N)
+% Swet = 2*Sref+Srest;
+Srest = 0.5; % surafce area of fuselage and tail
+
+% Range: R = (L/D)*h, L/Dmax at CD0 = kCl^2
+syms Sref
+SrefVec = 0.01:0.01:1;
+CD0 = Cfe*(2+Srest/Sref);
+CL_LDmax = sqrt(CD0/k);
+CL_LDmaxPlot = double(subs(CL_LDmax,Sref,SrefVec));
+CD_LDmax = 2*CD0;
+WTOI = rhoM*Sref + Wrest; % syms WTO
+v_LDmax = sqrt(2*WTOI/(CL_LDmax*rho*Sref));
+vRmaxPlot = double(subs(v_LDmax,Sref,SrefVec));
+Rmax = (CL_LDmax/CD_LDmax)*h;
+RmaxPlot = double(subs(Rmax,Sref,SrefVec)); % max range in terms of Sref
+% WTO = rhoM*SrefVec + Wrest; % total weight
+WTO = double(subs(WTOI,Sref,SrefVec)); % total weight
+RmaxWL = WTO./SrefVec; % wing loading for given Sref
+
+% Endurance: E = h/sink, sink = v*D/W, PRmin: CD0 = 1/3*k*CL^2
+syms Sref
+CD0 = Cfe*(2+Srest/Sref);
+CL_PRmin = sqrt(3*CD0/k);
+CL_PRminPlot = double(subs(CL_PRmin,Sref,SrefVec));
+CD_PRmin = CD0*4/3;
+WTOI = rhoM*Sref + Wrest; % syms WTO
+v_PRmin = sqrt(2*WTOI/(CL_PRmin*rho*Sref));
+vEmaxPlot = double(subs(v_PRmin,Sref,SrefVec));
+D_PRmin = CD_PRmin*1/2*rho*v_PRmin.^2*Sref;
+sink = v_PRmin*D_PRmin/WTOI;
+Emax = h/sink;
+EmaxPlot = double(subs(Emax,Sref,SrefVec));
+EmaxWL = WTO./SrefVec;
+
+% Finding stall AoA and CL limit
+e = 0.9;
+a0 = 0.1;
+alpha0 = 0;
+a = a0/(1+a0/(pi*AR*e));
+% CL = a*(alpha-alpha0);
+alphaStall = 8; % (rad), 8 or 9 degrees? 
+CLlim = a*(alphaStall-alpha0);
+
+figure(1)
+plot(RmaxWL,RmaxPlot)
+hold on
+plot(RmaxWL,70*ones(1,length(RmaxWL)))
+plot(RmaxWL,100*ones(1,length(RmaxWL)))
+ylabel('Range (m)')
+yyaxis right
+plot(RmaxWL,CL_LDmaxPlot)
+plot(RmaxWL,CLlim*ones(1,length(RmaxWL)))
+xlabel('Wing Loading (N/m^2)')
+ylabel('Coefficient of Lift Required')
+title('Maximum Range vs Wing Loading')
+legend('Range Achieved','Min Range Req','Max Range Req','CL Req for Range','CL Max Limit')
+
+figure(2)
+plot(EmaxWL,EmaxPlot)
+hold on
+plot(EmaxWL,7*ones(1,length(EmaxWL)))
+plot(EmaxWL,10*ones(1,length(EmaxWL)))
+ylabel('Endurance (s)')
+yyaxis right
+plot(EmaxWL,CL_PRminPlot)
+plot(EmaxWL,CLlim*ones(1,length(EmaxWL)))
+title('Maximum Endurance vs Wing Loading')
+xlabel('Wing Loading (N/m^2)')
+ylabel('Coefficient of Lift Required')
+legend('Endurance Achieved','Min Endurance Req',' Max Endurance Req','CL Req for Endurance','CL Max Limit')
+
+figure(3)
+plot(EmaxWL,vEmaxPlot)
+hold on
+plot(RmaxWL,vRmaxPlot)
+title('Velocity Required for Maximum Range and Endurance')
+xlabel('Wing Loading (N/m^2)')
+ylabel('Required Velocity (m/s)')
+legend('Velocity for Maximum Endurance','Velocity for Maximum Range')
+
+figure(4)
+plot(RmaxWL,WTO)
+title('Glider Weight vs Wing Loading')
+xlabel('Wing Loading (N/m^2)')
+ylabel('Glider Weight (N)')
+
+figure(5)
+% plot(RmaxWL,cost)
+title('Glider Cost vs Wing Loading')
+xlabel('Wing Loading (N/m^2)')
+ylabel('Glider Cost ($)')
+
+figure(6)
+title('Glider Wing Geometry vs Wing Loading')
+xlabel('Wing Loading (N/m^2)')
+ylabel('Length (m)')
+yyaxis right
+plot(RmaxWL,SrefVec)
+ylabel('Planform Area (m^2)')
